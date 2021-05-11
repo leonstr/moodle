7a0e7c10447718de9276ac466241a5438f08a509
Leon Stringer <leon.stringer@ntlworld.com>
2021-05-11 15:01:28 +0100

    MDL-71256 quizaccess_seb: Can change after attempt
    Add a new setting: 'quizaccess_seb | lockconfigafterattempt'.  By
    default this is on and the Safe Exam Browser settings cannot be changed
    once a quiz has been attempted.  Turning this setting off allows SEB
    settings to be changed, with a warning message saying that attempts have
    been made.

--- mod/quiz/accessrule/seb/db/upgrade.php
+++ mod/quiz/accessrule/seb/db/upgrade.php
@@ -43,5 +43,9 @@ function xmldb_quizaccess_seb_upgrade($oldversion) {
     // Automatically generated Moodle v3.10.0 release upgrade line.
     // Put any upgrade step following this.
 
+    if ($oldversion < 2020110901) {
+        set_config('lockconfigafterattempt', 1, 'quizaccess_seb');
+    }
+
     return true;
 }
